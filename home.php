e0752c1faea9c3b8d6f8713eac182bd07214418c
Ariful2016 <69759406+Ariful2016@users.noreply.github.com>
2020-08-20 17:55:54 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html>
+<head><title>Home</title>
+<style>
+div{
+   background-color : #3BC955;
+}
+div.Foot{
+   float:left;
+   width:100%;
+background-color:#626657;
+}
+#scrollbar {
+   background-color:#D3ECF5;
+   height:40px;
+}  
+ul{
+   margin:0;
+   padding:0;
+   list-style:none;
+}
+ul li{
+   float:left;
+   width:200px;
+   height:40px;
+   background-color:#BAA040;
+   position:relative;
+   text-align:center;
+   line-height:40px;
+   font-size:20px;
+}
+ul li a{
+   align: canter;
+   display:block;
+}
+ul li a:hover{
+    background-color:#C9DAAF;
+    width:200px;
+   height:40px;
+}
+ul li ul li{
+    display : none;
+}
+ul li:hover ul li{
+     display : block;
+}
+.amc{
+   width:500px;
+   border:50px auto;
+   position:absolute;
+   right:-250px;
+}
+.search{
+   padding:1px; 
+   border:1px solid orange;
+   background-color:white;
+   position:absolute;
+   left:-5;
+}
+.search:hover{
+   background-color:lightgray;
+}
+  .newclock{
+	  height:500px;
+	  width:500px;
+	  background-color:#000;
+	  bottom:-1px;
+	  right:0px;
+	  position:absolute;
+	  display:inline-block;
+  }
+ .clock{
+	   position : absolute;
+	   top : 50%;
+	   left:50%;
+	   transform: translateX(-50%) translateY(-50%);
+	   color: #3deb60;
+	   font-size:80px;
+	   border:3px solid #f7a540;
+	   padding:0px 5px 0px 5px;
+	 }
+</style>
+</head>
+<body>
+<header>
+<div>
+<table>
+<tr><td>
+<img src = "Capture.PNG" width="100px" height = "50px">
+</td><td>
+<h2 style="color:white">Online College Admission Predector</h2></td></tr>
+</table>
+<div>
+   <form class="amc">
+      <input type="text"placeholder="Search..."required>
+      <button class="search">&#x1f50d</button>
+   </form>
+</div>
+</div>
+</header>
+<div id="scrollbar">
+<ul>
+<li><a href="#home" style="text-decoration:none;color:#BE2AF4">Home</a></li>
+<li><a href="#admission" style="text-decoration:none;color:#BE2AF4">Admission</a>
+ <ul>
+    <li><a href="#hsc" style="text-decoration:none;color:#BE2AF4">HSC</a></li>
+	 <li><a href="#hons" style="text-decoration:none;color:#BE2AF4">Honurs</a></li>
+	  <li><a href="#Mast" style="text-decoration:none;color:#BE2AF4">Masters</a></li>
+ </ul>
+</li>
+<li><a href="#port" style="text-decoration:none;color:#BE2AF4">Portfolio</a></li>
+<li><a href="#contact" style="text-decoration:none;color:#BE2AF4">Contact</a></li>
+</ul>
+</div>
+<div style="background-color:#D9F5EF;height:900px;width:500px;float:left;width:75%">
+<h2 style="color:blue">What is College Admission Predictor?</h2>
+<h3>Please read the information bellow carefully</h3>
+<p>
+
+
+This project Engineering Admission Predictor System is web based 
+application in which students<br> can register with their personal as well as 
+marks details for prediction the admission in colleges and the <br>administrator 
+can allot the seats for the students. Administrator can add the college <br>
+details and he batch details. Using this software, the entrance seat <br>
+allotment became easier and can be implemented using system. The main 
+advantage of the<br> project is the computerization of the entrance seat 
+allotment process.<br> Administrator has the power for the allotment. He can <br>
+add the allotted seats into a file and the details are saved into the system. The total time for
+<br> the entrance allotment became lesser and the allotment process became 
+faster.[1]
+
+<br><br><br>
+<b>Features:</b><br>
+<p>It helps students to get the list of colleges to which they can apply as the system<br> shortlists the colleges by comparing the student’s marks and college's cut off.<br>
+<b>Admin</b> manages all the system related task such as adding college details,<br> specifying cut-off details, managing college details, viewing student’s details and<br> viewing feedback from registered students.<br>
+<b>Student</b> can register themselves using web portal by entering their personal as<br> well as marks details. Once registration is successful, they can login and view<br> colleges allotted based on their marks.
+</p>
+<h4>Advantages:</h4>
+<ol>
+<li>It helps student for making decision for choosing a right college.</li>
+<li>Here the chance of occurrence of error is less when compared with the existing system.</li>
+<li>It is fast, efficient and reliable</li>
+<li>Avoids data redundancy and inconsistency</li>
+<li>Very user-friendly</li>
+<li>Easy accessibility of data</li>
+<li>Number of personnel required is considerably less</li>
+<li>Provides more security and integrity to data.</li>
+</ol>
+
+
+<h4 style="color:red">
+Warning:
+</h4>
+<h5>Please insert datas carefully</h5>
+<p>
+<b>References:</b><br>
+1.<a href="https://nevonprojects.com/college-admission-predictor-php/">https://nevonprojects.com/college-admission-predictor-php/</a>Accessed:02/02/2019<br>
+2.<a href="http://synopsis.nevemtech.com/index.aspx?Id=P063">http://synopsis.nevemtech.com/index.aspx?Id=P063.doc/</a>Accessed:02/02/2019
+</p>
+</div>
+
+<div  style="background-color:#C1F5C7;height:500px;width:300px;width:25%;float:right;">
+<table>
+<tr><td></td>
+<td><h2 style="color:#27F7DD">subscribe</h2></td></tr>
+<tr><td></td>
+<td><a href="https://htmlcolorcodes.com">NUdaodd</a></td></tr>
+<tr><td></td>
+<td><a href="https://en.wikipedia.org/wiki/Text_messaging">nodjadio</a><td></tr>
+<tr><td></td>
+<td><h2 style="color:#27F7DD">Popular item</h2></td></tr>
+<tr><td></td>
+<td><a href="http://www-db.deis.unibo.it/courses/TW/DOCS/w3schools/html/html_links.asp.html">Ldahofiadfjakldfjaoidjf<br>
+daodkijfaf</a></td></tr>
+<tr><td></td>
+<td><h2 style="color:#27F7DD">Attribute</h2></td></tr>
+<tr><td></td>
+<td><a href="https://www.tutorialspoint.com/php/php_syntax_overview.htm">aioedfadkadfkljadfh<br>dakdadafsdf</a></td></tr>
+<tr><td></td>
+<td><button type="button" style="background-color:#A1D916;height:50px;width:100px;">Subscribe</button></td></tr>
+</table>
+</div>
+<div class = "newclock">
+   <div class="dclock">
+     <div id="clock"class="clock"></div>
+	 <script type="text/javascript">
+	    function showTime(){
+		   var date = new Date();
+		   var h = date.getHours();
+		   var m = date.getMinutes();
+		   var s = date.getSeconds();
+		   var t = "AM"
+		   if(h==0){
+		      h = 12;
+		   }
+		   if(h>12){
+		      h = h - 12;
+			  t = "PM";
+		   }
+		   if(h<10){
+		      h = "0" + h;
+		   }
+		   if(m<10){
+		      m = "0" + m;
+		   }
+		   if(s<10){
+		      s = "0" + s;
+		   }
+		   var time = h + ":" + m + ":" + s +" " + t;
+		   document.getElementById("clock").innerText = time;
+		   document.getElementById("clock").textContent = time;
+		   setTimeout(showTime,1000);
+		}
+		showTime();
+		
+		
+	 </script>
+	</div>
+</div>
+<footer>
+<div class="Foot">
+<p>Copyright &copy 2008 PSDTubs</p>
+<p>All right Reserved</p>
+</div>
+</footer>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head><title>Login page</title>
+<style>
+.headi{
+   background-color : #3BC955;
+}
+body{
+	background-color: #00ffaa;
+}
+div.Foot{
+   float:left;
+   width:100%;
+background-color:#626657;
+}
+.concord{ 
+    height:500px;
+    width:900px;
+    margin:14px;
+    padding:30px;
+    border:4px;
+    border-color:rgb(3, 139, 254);
+}
+.str{
+	padding:20px;
+	margin:10px;
+}
+.catin{
+	padding:20px;
+	margin:10px;
+}
+.info{
+	align:center;
+	height:100px;
+	width:400px;
+	margin:20px;
+	padding:200px;
+}
+.submit{
+	 height:40px;
+     width:100px;
+    <! background-color:blue;>
+     border:2px;
+     border-color:rgb(105, 79, 200);
+     color:black;
+     font-size:20px bold;
+}
+</style>
+</head>
+<body>
+<header>
+<div class="headi">
+<table>
+<tr><td>
+<img src = "Capture.PNG" width="100px" height = "50px">
+</td><td>
+<h2 style="color:white">Online College Admission Predector</h2></td></tr>
+</table>
+</div>
+</header>
+<div class="concord">
+<form class="info">
+<table>
+<tr><td>Email : </td><td>
+<input type="text"placeholder="email@example.com"style="font-size:200%"class="str"required></td></tr>
+<tr><td>Password: </td><td>
+<input type="password"placeholder="password"style="font-size:200%"class="catin"required></td></tr>
+<tr><td></td><td>
+<input type="submit"value="sbmit"class="submit"></td>
+</tr>
+</table>
+</form>
+</div>
+<footer>
+<div class="Foot">
+<p>Copyright &copy 2008 PSDTubs</p>
+<p>All right Reserved</p>
+</div>
+</footer>
+</body>
+
+</html>
